52aef87c155c945c9aa02d8ef0d9c7347a5821d6
Preeti <preeti@binarylogicit.com>
2024-06-29 15:48:53 -0400

    app.js file changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header>This is Header</header>
-      <h2>Inside App.js</h2>
+      <h2>{process.env.WELCOME_TEXT || "Inside App.js"}</h2>
       <img src="logo92.png" alt="Logo"></img>
       <footer>This is footer</footer>
     </div>
