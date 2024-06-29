6833f94d512dff284107e3fce40d6df742e79c46
Preeti <preeti@binarylogicit.com>
2024-06-29 14:43:40 -0400

    added image

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header>This is Header</header>
       <h2>Inside App.js</h2>
+      <img src="logo92.png" alt="Logo"></img>
       <footer>This is footer</footer>
     </div>
   );
